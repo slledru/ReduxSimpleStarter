956ed27aa1fbc1f8eb3c61fb8799ade899d8f94c
slledru <slledru@msn.com>
2018-05-06 17:42:07 -0600

    connected component with app state

--- src/containers/book_detail.js
+++ src/containers/book_detail.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class BookDetail extends Component {
+class BookDetail extends Component {
   render() {
     return (
       <div>Book Detail!</div>
     )
   }
 }
+
+function mapStateToProps(state) {
+  // Whatever is returned will show up as props
+  // inside of BookList
+  return {
+    book: state.activeBook
+  }
+}
+
+export default connect(mapStateToProps)(BookDetail)
