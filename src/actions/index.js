0d26a38f659991dbd97febab782f51cc819bafcc
slledru <slledru@msn.com>
2018-05-07 09:06:07 -0600

    corrected URL to resolve CORS

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API_KEY = '4ad044eab04d23154e3fa874df93ead0'
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
