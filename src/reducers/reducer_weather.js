e833279dfe8200bc44f83bca8becbde980686e8f
slledru <slledru@msn.com>
2018-05-07 09:39:42 -0600

    weather reducer connected to action creator

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,9 @@
+import FETCH_WEATHER from '../actions/index'
+
 export default function(state = null, action) {
-  console.log('Action received', action)
+  switch (action.type) {
+    case FETCH_WEATHER:
+
+  }
   return state
 }
