5ad841d0707d3d1415ee45f82fb1b0c78275a6b0
slledru <slledru@msn.com>
2018-05-07 08:37:15 -0600

    using axios in action creator to send ajax calls to weather API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,16 @@
-const API_KEY = '4ad044eab04d23154e3fa874df93ead0'
+import axios from 'axios'
 
-export default function() {
-  
+const API_KEY = '4ad044eab04d23154e3fa874df93ead0'
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`
+  const request = axios.get(url) // returns Promise
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
 }
